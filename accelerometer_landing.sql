b13f29f5ffb4c87eaec8862cb57e21eb3293a50e
hanklee2018 <130482370+hanklee2018@users.noreply.github.com>
2023-06-14 21:39:02 -0400

    add created scripts from AWS

---
+++ accelerometer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landng` (
+  `user` string,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://mybucket-whl/accelerometer/landing/'
+TBLPROPERTIES ('classification' = 'json');
---
+++ trainer_trusted_to_curated.py
@@ -0,0 +1,66 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node S3 bucket
+S3bucket_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="step_trainer_landing",
+    transformation_ctx="S3bucket_node1",
+)
+
+# Script generated for node Amazon S3
+AmazonS3_node1686788905036 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_curated",
+    transformation_ctx="AmazonS3_node1686788905036",
+)
+
+# Script generated for node Privacy Join
+PrivacyJoin_node1686789028926 = Join.apply(
+    frame1=AmazonS3_node1686788905036,
+    frame2=S3bucket_node1,
+    keys1=["serialnumber"],
+    keys2=["serialnumber"],
+    transformation_ctx="PrivacyJoin_node1686789028926",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1686789171897 = DropFields.apply(
+    frame=PrivacyJoin_node1686789028926,
+    paths=[
+        "customername",
+        "email",
+        "phone",
+        "birthday",
+        "registrationdate",
+        "lastupdatedate",
+        "sharewithresearchasofdate",
+        "sharewithpublicasofdate",
+    ],
+    transformation_ctx="DropFields_node1686789171897",
+)
+
+# Script generated for node S3 bucket
+S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
+    frame=DropFields_node1686789171897,
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "path": "s3://mybucket-whl/step_trainer/trusted/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="S3bucket_node3",
+)
+
+job.commit()
